21336259e4378694b7c3e8ad146ac7c0e7e9be0b
MateK23 <32340202+MateK23@users.noreply.github.com>
2020-10-22 21:00:56 +0400

    Add files via upload

---
+++ storage/framework/views/93967f622dbaeb4798a3d7dc9707183beafa88b1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Document</title>
+</head>
+	<body>
+		<h1>test page</h1>
+		<?php echo $__env->yieldContent('content'); ?>
+
+
+		
+	</body>
+</html><?php /**PATH C:\xampp\htdocs\main_project\resources\views/index.blade.php ENDPATH**/ ?>
